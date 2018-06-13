1976f64bb34fb6359828cdacad3f89049f091717
Walter Morales <walkter@gmail.com>
2018-06-12 20:37:34 -0600

    Updated Main

--- main.c
+++ main.c
@@ -3,6 +3,40 @@
 
 int main()
 {
+    
+    DoublyGenericList *DList = new_DoublyGenericList();
+
+    Info *Inf_0 = (Info*) malloc(sizeof(Info));
+    Inf_0->ID = 10;
+    Inf_0->Name = "Walter";
+
+    Info *Inf_1 = (Info*) malloc(sizeof(Info));
+    Inf_1->ID = 10;
+    Inf_1->Name = "Armando";
+
+    Info *Inf_2 = (Info*) malloc(sizeof(Info));
+    Inf_2->ID = 10;
+    Inf_2->Name = "Alejandro";
+
+    Info *Inf_3 = (Info*) malloc(sizeof(Info));
+    Inf_3->ID = 10;
+    Inf_3->Name = "Pamela";
+
+    Info *Inf_4 = (Info*) malloc(sizeof(Info));
+    Inf_4->ID = 10;
+    Inf_4->Name = "Raul";
+
+    FrontInsert(DList,Inf_0);
+    FrontInsert(DList,Inf_1);
+    FrontInsert(DList,Inf_2);
+    FrontInsert(DList,Inf_3);
+    FrontInsert(DList,Inf_4);
+
+    GeNode *TmpNode = getNodebyIndex(DList,0);
+    int k = 0;
+    
+    
+    /*
     DoubleList * ML = NewDoubleList();
     Student * Perito = NewStudent();
 
@@ -50,4 +84,5 @@ int main()
 
     printf("Hello World!\n");
     return 0;
+    */
 }
